9d7ccc5d2977158c1a6519cadd6858e535042cd9
Aleksandyr94 <aleksandyr.kamenov@gmail.com>
2017-07-02 19:50:09 +0300

    add some data methods

--- 02.book-app/src/data/dataMethods.js
+++ 02.book-app/src/data/dataMethods.js
@@ -1,5 +1,11 @@
+import FakeData from './fakeData'
+
 export default class Data {
   static getAllRestaurants () {
-    
+    return FakeData.Restaurants
+  }
+
+  static allAuthors () {
+    return FakeData.Authors
   }
 }
