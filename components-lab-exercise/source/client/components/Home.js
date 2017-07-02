2e287be4e4933e44876780fff297bb0c4a64d950
Aleksandyr94 <aleksandyr.kamenov@gmail.com>
2017-07-02 16:01:05 +0300

    add navbar

---
+++ components-lab-exercise/source/client/components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        And I am Home component
+      </div>
+    )
+  }
+}
+
+export default Home
--- components-lab-exercise/source/client/routes.js
+++ components-lab-exercise/source/client/routes.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React from 'react'
+import { Route } from 'react-router'
+import Home from './components/Home'
 
-import App from './components/App';
+import App from './components/App'
 
 export default (
-    <Route component={ App } />
-);
+  <Route component={App}>
+    <Route path='/' component={Home} />
+  </Route>
+)
