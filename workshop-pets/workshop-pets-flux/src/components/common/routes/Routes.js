cf3268e481a95d2d0e64bc734800c892bb5ce438
Aleksandar Kamenov <AKamenov@infragistics.com>
2017-07-14 11:30:00 +0300

    add logout functionality

--- workshop-pets/workshop-pets-flux/src/components/common/routes/Routes.js
+++ workshop-pets/workshop-pets-flux/src/components/common/routes/Routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import ListPetsPage from '../../pets/ListPetsPage'
 import RegisterUserPage from '../../users/RegisterUserPage'
 import LoginUserPage from '../../users/LoginUserPage'
+import LogoutPage from '../../users/LogoutPage'
 
 const Routes = () => (
   <Switch>
     <Route path='/' exact component={ListPetsPage} />
-    <Route path='/users/register' exact component={RegisterUserPage} />
-    <Route path='/users/login' exact component={LoginUserPage} />
+    <Route path='/users/register' component={RegisterUserPage} />
+    <Route path='/users/login' component={LoginUserPage} />
+    <Route path='/users/logout' component={LogoutPage} />
   </Switch>
 )
 
